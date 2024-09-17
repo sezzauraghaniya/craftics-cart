5d1aedde087d74f72aa655a0372c9a69217e21fc
sezzauraghaniya <sezza.auraghaniya>
2024-09-17 11:25:36 +0700

    fix null default di descriptions

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Craft(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
-    description = models.TextField(max_length=2000)
+    description = models.TextField(max_length=2000, default='')
 
     def __str__(self):
         return self.name
