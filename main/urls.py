e32b97b74c932c7f6b53dd7c19089b808f237a6b
sezzauraghaniya <sezza.auraghaniya>
2024-09-19 22:53:46 +0700

    push tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 from uuid import UUID
-from main.views import show_main
 from main.views import show_main, create_craft_entry, show_xml, show_json, show_json_by_id, show_xml_by_id
+from main.views import login_user, register, logout_user
+
 
 app_name = 'main'
 
@@ -14,5 +15,8 @@ urlpatterns = [
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('create-craft-entry', create_craft_entry, name='create_craft_entry'),
     path('craft/delete/<uuid:pk>/', views.delete_craft, name='delete_craft'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from main.forms import CraftEntryForm
 from main.models import Craft
-from django.http import HttpResponse
+import datetime
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     # fetch objects from database
-    craft_entries = Craft.objects.all()
+    craft_entries = Craft.objects.filter(user=request.user)
 
     context = {
-        'nama' : 'Sezza Auraghaniya Winanda',
+        'nama' : request.user.username,
         'kelas': 'PBP F',
 
         'crafts': [
@@ -18,7 +26,8 @@ def show_main(request):
             {'name': 'Clay Rings', 'price': 5.000, 'description': 'Cincin tanah liat warna-warni.'},
         ],
 
-        'craft_entries' : craft_entries
+        'craft_entries' : craft_entries,
+        'last_login': request.COOKIES['last_login']
     }
 
     return render(request, "main.html", context)
@@ -27,7 +36,9 @@ def create_craft_entry(request):
     form = CraftEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        craft_entry = form.save(commit=False)
+        craft_entry.user = request.user
+        craft_entry.save()
         return redirect('main:show_main')
     
     context = {'form': form}
@@ -53,3 +64,40 @@ def delete_craft(request, pk):
     craft = get_object_or_404(Craft, pk=pk)
     craft.delete()
     return redirect('main:show_main')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New account created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
+
